aacd3eb8ceba084aef8cea6606c4bd97dcf1edf5
yamiln82 <yamilnu82@gmail.com>
2024-06-22 10:31:14 -0500

    Proyecto inicial completo

--- src/Component.js
+++ src/Component.js
@@ -1,7 +1,24 @@
-import React from 'react'
+
+import React, { useState } from 'react'
 
 export default function Component() {
+  const[text, setText]= useState()
+  const[updated, setUpdated]= useState()
+
+  const textOnchange = (event) => {
+    setText(event.target.value)
+  }
+
+  const buttonOnClick = () => {
+    setUpdated(text)
+  }
+  
   return (
-    <div>My Component </div>
+    <div>
+      <input type="text"  value={text} onChange={textOnchange}/>
+      <button onClick={buttonOnClick}>Actualizar</button>
+      <p>texto del input: {text}</p>
+      <p>Texto Actualizado: {updated}</p>
+       </div>
   )
 }
